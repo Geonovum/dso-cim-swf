1bf9f12f0fa5dee1d7d05e5e8d21ee47980c4a1a
Wilko Quak <w.quak@geonovum.nl>
2023-10-25 09:51:00 +0200

    wijzigingen in template doorgevoerd.

--- process-template.sh
+++ process-template.sh
@@ -4,6 +4,8 @@
 #
 cat ../dso-cim-template/README.md.template | sed -f configuration.sed > README.md
 cat ../dso-cim-template/abstract.md.template | sed -f configuration.sed > abstract.md
-cat ../dso-cim-template/js/config.js.template | sed -f configuration.sed > js/config.js
 cat ../dso-cim-template/imvertor/Imvertor.properties.template | sed -f configuration.sed > imvertor/Imvertor.properties
 cat ../dso-cim-template/index.html.template | sed -f configuration.sed > index.html
+
+cp ../dso-cim-template/03-Toepassingsgebied.md .
+
